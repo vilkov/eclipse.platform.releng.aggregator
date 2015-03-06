5f374fca67b7c207923b724be9f9a93ffc530fc9
David Williams <david_williams@us.ibm.com>
2015-03-06 15:17:28 -0500

    Bug 431116 - Releases for Mac OS X should be bundled as a proper "Mac App" and/or "Library"
    X-Build, using forked version of Tycho.

--- production/buildTycho.sh
+++ production/buildTycho.sh
@@ -1,14 +1,28 @@
-#!/bin/sh
-git clone http://git.eclipse.org/gitroot/tycho/org.eclipse.tycho.git
+#!/usr/bin/env bash
+
+LOCAL_REPO=${LOCAL_REPO:-${PWD}/localRepo}
+TYCHO_MVN_ARGS="-Dmaven.test.skip=true -Dmaven.repo.local=$LOCAL_REPO -Dtycho.localArtifacts=default"
+echo -e "\n\tTYCHO_MVN_ARGS\n"
+
+
+
+if [[ ! -d org.eclipse.tycho ]]
+then
+  git clone git://git.eclipse.org/gitroot/tycho/org.eclipse.tycho.git
+fi
 cd org.eclipse.tycho
-git fetch git://git.eclipse.org/gitroot/tycho/org.eclipse.tycho
+git pull git://git.eclipse.org/gitroot/tycho/org.eclipse.tycho
 refs/changes/74/43274/1 && git checkout FETCH_HEAD
 git fetch git://git.eclipse.org/gitroot/tycho/org.eclipse.tycho
 refs/changes/28/43328/1 && git cherry-pick FETCH_HEAD
 git revert 2ab08b7a0079f277f52eacab3a9a4bbd0b112564 --no-edit
 # dw, removed: -Dtycho.localArtifacts=ignore
-mvn clean install -Dmaven.test.skip=true -Dmaven.repo.local=$LOCAL_REPO -Dtycho.localArtifacts=default
+mvn ${TYCHO_MVN_ARGS} clean install
 cd ..
-git clone http://git.eclipse.org/gitroot/tycho/org.eclipse.tycho.extras.git
+if [[ !-d org.eclipse.tycho.extras ]]
+then
+  git clone git://git.eclipse.org/gitroot/tycho/org.eclipse.tycho.extras.git
+fi
 cd org.eclipse.tycho.extras
-mvn clean install -Dmaven.repo.local=$LOCAL_REPO -Dtycho.localArtifacts=default
+git pull git://git.eclipse.org/gitroot/tycho/org.eclipse.tycho.extras.git
+mvn ${TYCHO_MVN_ARGS} clean install
