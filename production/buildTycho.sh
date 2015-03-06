cfcad957a86ac8ebc441c05dd03cab4336cb2764
David Williams <david_williams@us.ibm.com>
2015-03-06 13:59:44 -0500

    Bug 431116 - Releases for Mac OS X should be bundled as a proper "Mac App" and/or "Library"
    X-Build, using forked version of Tycho.

--- production/buildTycho.sh
+++ production/buildTycho.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 git clone http://git.eclipse.org/gitroot/tycho/org.eclipse.tycho.git
 cd org.eclipse.tycho
-git fetch git://git.eclipse.org/gitroot/tycho/org.eclipse.tycho 
+git fetch git://git.eclipse.org/gitroot/tycho/org.eclipse.tycho
 refs/changes/74/43274/1 && git checkout FETCH_HEAD
-git fetch git://git.eclipse.org/gitroot/tycho/org.eclipse.tycho 
+git fetch git://git.eclipse.org/gitroot/tycho/org.eclipse.tycho
 refs/changes/28/43328/1 && git cherry-pick FETCH_HEAD
 git revert 2ab08b7a0079f277f52eacab3a9a4bbd0b112564 --no-edit
 # dw, removed: -Dtycho.localArtifacts=ignore
-mvn clean install -Dmaven.test.skip=true 
+mvn clean install -Dmaven.test.skip=true -Dmaven.repo.local=$LOCAL_REPO
 cd ..
 git clone http://git.eclipse.org/gitroot/tycho/org.eclipse.tycho.extras.git
 cd org.eclipse.tycho.extras
-mvn clean install
+mvn clean install -Dmaven.repo.local=$LOCAL_REPO
